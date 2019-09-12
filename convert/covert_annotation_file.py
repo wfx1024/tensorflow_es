79fecce574425651668d81a3cf715df6440cad52
windandwine <1975025563@qq.com>
2019-09-12 11:41:54 +0800

    wider face annotation convert to coco annoatation format

--- convert/covert_annotation_file.py
+++ convert/covert_annotation_file.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import os
 import cv2
+from tqdm import trange, tqdm
+import time
 
 """
 修改Wider Face数据集注解文件格式，并保存
@@ -53,57 +55,101 @@ invalid:
 
 """
 现格式:
-File name x1, y1, w, h, 21,  x1, y1, w, h,21 ...
-21是face类别
+File name x1, y1, w, h, label, x1, y1, w, h, label ...
+lable是类别index
 """
 
 
-def draw(image_list, src_img_dir=None, tar_img_dir=None):
-    if not os.path.exists(tar_img_dir):
-        os.mkdir(tar_img_dir)
-    for item in image_list:
-        sub_path = item["path"]
-        path_seg = sub_path.split("/")
-        # path = os.path.join(src_img_dir, sub_path)
-        path = src_img_dir + '/' + sub_path
-        boxes = item["boxes"]
-        img = cv2.imread(path)
-        # cv2.imshow('sd', img)
-        # cv2.waitKey(0)
-        for box in boxes:
-            ord = box.split(" ")
-            x, y, w, h = int(ord[0]), int(ord[1]), int(ord[2]), int(ord[3])
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.imshow('sd', img)
-        cv2.waitKey(0)
-        tar_dir = os.path.join(tar_img_dir, path_seg[0])
-        if not os.path.exists(tar_dir):
-            os.mkdir(tar_dir)
-        tar_path = os.path.join(tar_dir, path_seg[1])
-        cv2.imwrite(tar_path, img)
+def draw(annotation, src_img_dir=None):
+    """
+    画框并展示
+    :param annotation:
+    :param src_img_dir:
+    :return:
+    """
+
+    sub_path = annotation["path"]
+    path = src_img_dir + '/' + sub_path
+    boxes = annotation["boxes"]
+    img = cv2.imread(path)
+    for box in boxes:
+        ord = box.split(" ")
+        x, y, w, h = int(ord[0]), int(ord[1]), int(ord[2]), int(ord[3])
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow('img_detect', img)
 
 
-def parse(label_file_path, src_img_dir, tar_img_dir):
+def parse(label_file_path, src_img_dir):
+    """
+    提取每个图片所有标注
+    :param label_file_path:
+    :param src_img_dir:
+    :return:
+    """
     fr = open(label_file_path, 'r')
-    image_list = []
     line = fr.readline().rstrip()
-    # while line:
-    mdict = {}
-    path = line
-    mdict["path"] = path
-    num = fr.readline().rstrip()
-    boxes_list = []
-    for n in range(int(num)):
-        box = fr.readline().rstrip()
-        boxes_list.append(box)
-    mdict["boxes"] = boxes_list
-    image_list.append(mdict)
-    # line = fr.readline().rstrip()
-    draw(image_list, src_img_dir, tar_img_dir)
+    while line:
+        annotation = {}
+        path = line
+        annotation["path"] = path
+        num = fr.readline().rstrip()
+        boxes_list = []
+        for n in range(int(num)):
+            box = fr.readline().rstrip()
+            boxes_list.append(box)
+        annotation["boxes"] = boxes_list
+        draw(annotation, src_img_dir)
+        if cv2.waitKey(1000) & 0xFF == ord('q'):
+            break
+        line = fr.readline().rstrip()
+
+
+def convert_annotation(label_file_path, to_file_path, source_img_dir):
+    """
+    解析写出
+    :param label_file_path:
+    :param to_file_path:
+    :param source_img_dir
+    :return:
+    """
+    lines = len(open(label_file_path, 'r').readlines())
+    fr = open(label_file_path, 'r')
+    fw = open(to_file_path, 'w')
+
+    img_num = 0
+    pbar = tqdm(total=lines)
+    for _ in range(lines):
+        step = 1  # 进度条步数
+        line = fr.readline().rstrip()
+        if line:
+            img_num += 1  # 图片数
+            path = line  # 路径
+            img_path = source_img_dir + '/' + path
+            img = cv2.imread(img_path)
+            shape = img.shape
+            fw.write(path + ' ')
+            fw.write(str(shape[1]) + ' ' + str(shape[0]) + ' ')
+            num = fr.readline().rstrip()  # bbox数量
+            step += 1
+            for n in range(int(num)):  # 每个bbox
+                box = fr.readline().rstrip().split()  # 每个bbox
+                step += 1
+                for j in range(4):
+                    fw.write(box[j] + ' ')
+                fw.write('81 ')
+            fw.write('\n')
+        pbar.update(step)  # 更新进度条
+    print("\033[32m转换完成，共有标注图片{}个".format(img_num))
+
+
+def main():
+    file_path = "../data/sample/wider_face_val_bbx_gt.txt"  # 注解文件
+    source_img_dir = "D:/dl_data/sample/images"  # 对应的图片文件路径
+    to_file_dir = '../data/sample/wider_face_val_bbx_gt2.txt'  # 写入文件路径
+    # target_img_dir = "data"  # 画框后图片路径
+    parse(file_path, source_img_dir)  # 读取画框所有图像
+    # convert_annotation(file_path, to_file_dir, source_img_dir)  # 转换convert
 
 
 if __name__ == '__main__':
-    file_path = "F:/sample_data2/WIDERFace/wider_face_split/wider_face_train_bbx_gt.txt"
-    source_img_dir = "F:/sample_data2/WIDERFace/WIDER_train/images"
-    target_img_dir = "data"
-    parse(file_path, source_img_dir, target_img_dir)
+    main()
--- utils/dataset_utils.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import tensorflow as tf
-from utils.data_utils import get_batch_data
-from setting import train_args as args
-
-"""
-构建dataset方法，tf.data相关
-"""
-
